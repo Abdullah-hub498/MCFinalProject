450a064d0673c12aacf7f2e4c7fa995f95bb1424
Abdullah-hub498 <abidabdullah498@gmail.com>
2021-05-29 10:36:48 +0500

    New activites added and sign Up page making

--- app/src/main/java/com/example/mcfinalproject/MainActivity.java
+++ app/src/main/java/com/example/mcfinalproject/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.mcfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent categoryIntent= new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(categoryIntent);
+                finish();
+            }
+        });
     }
 }
